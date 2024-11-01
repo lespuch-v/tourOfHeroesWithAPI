ff4c60e11a706b44d4296ab5c372b901f85faa11
lespuch-v <v.lespukh@gmail.com>
2024-11-01 22:31:01 +0100

    form development

--- tourofheroeswithapi.client/src/app/models/models.ts
+++ tourofheroeswithapi.client/src/app/models/models.ts
@@ -2,6 +2,7 @@ export interface Hero {
   id: number;
   name: string;
   type: 'strength' | 'intelligence' | 'magic' | 'speed' | 'technology';
+  superpower: string;
   rating: number;
   health: number;
   description: string;
